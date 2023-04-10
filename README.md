639585d0d45202ccc827739e1f4fb2382b6423e3
Jesús Escribano Serena <jesusescribano2002@gmail.com>
2023-04-10 23:26:02 +0200

    Adding the Arduino source code

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# pulsera-IMIBIC
-Aquí se almacena el código de una puslera para control de pacientes en tratamiento de salud mental para el Instituto Maimónides de Investigación Biomédica de Córdoba
+# Pulsera control para la Salus Mental
+
+## Diseño de la pulsera
+
+En primer lugar, vamos a ver como podemos recoger una salida digital que nos da un botón en Arduino, para ello debemos conectar una de las patillas a la entrada de 5V, a una resistencia y a la salida digital que queramos capturar (imagen de la izquierda):
+
+![Conexión de botón al arduino](botonArduino.webp)
+
+Vamos a necesitar 4 botones ya que se nos piden 4 estados (solo tenemos dos botones):
+
+- Estoy teniendo una crisis
+- Siento que la medicación no me está yendo bien
+- Tengo pensamientos suicidas (SOS)
+- 
---
+++ arduinoPulsera/arduinoPulsera.ino
@@ -0,0 +1,69 @@
+/*
+PROGRAMA PARA LA IMPLEMENTACIÓN DE UNA PULSERA INTELIGENTE QUE AYUDE A ENFERMOS DE SALUD MENTAL PARA SU SEGUIMIENTO 
+@author Jesús Escribano Serena
+@author Rafael Emilio Pérez López
+*/
+
+#include "SoftwareSerial.h"
+
+const int idPaciente = 1001;
+
+const int botonMedicacion = 1;
+const int botonSOS = 2;
+const int botonCrisis = 3;
+
+int estaMedicacion = HIGH;
+int estaSOS = HIGH;
+int estaCrisis = HIGH;
+
+SoftwareSerial mySerial(3, 2); //SIM800L Tx & Rx is connected to Arduino #3 & #2
+
+void setup() {
+  pinMode(botonMedicacion, INPUT);
+  pinMode(botonSOS, INPUT);
+  pinMode(botonCrisis, INPUT);
+  String Mensaje = "";
+  mySerial.begin(9600);
+  delay(1000);
+
+}
+
+void loop() {
+  estaMedicacion = digitalRead(botonMedicacion);
+  estaSOS = digitalRead(botonSOS);
+  estaCrisis = digitalRead(botonCrisis);
+
+  if (estaCrisis == LOW){
+    Mensaje = "PACIENTE ID: "+idPaciente+ " ESTADO: Crisis";
+    enviarMensaje(mensaje);
+  }
+  elseif(estaSOS == LOW){
+    Mensaje = "PACIENTE ID: "+idPaciente+ " ESTADO: PensamientosSuicidas";
+  }
+  elseif(estaMedicacion == LOW){
+    Mensaje = "PACIENTE ID: "+idPaciente+ " ESTADO: FalloMedicación";
+  }
+
+}
+
+void enviarMensaje(String mensaje){
+  mySerial.print("AT");
+  updateSerial();
+  mySerial.print("AT+CMGF=1");
+  updateSerial();
+  mySerial.print("AT+CMGS=\"+34682479236\"");
+  updateSerial();
+  mySerial.print(mensaje);
+  updateSerial();
+  mySerial.write(26);
+}
+
+void updateSerial(){
+  delay(500);
+  while (Serial.available()){
+    mySerial.write(Serial.read());
+  }
+  while(mySerial.available()){
+    Serial.write(mySerial.read())
+  }
+}
